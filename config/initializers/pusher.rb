3947d4f0dbafee9962b592e39ae8c4d2115be238
Keith Tang <keithsktang@gmail.com>
2015-11-24 16:36:03 -0500

    changed pusher keys again

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -9,9 +9,8 @@ require 'pusher'
 # Pusher.key = '9a29dc21625531215e18'
 # Pusher.secret = 'b07b3be6f2f24a87c5de'
 
-#KEYS FOR DEVELOPMENT (HEROKU)
-Pusher.app_id = '156779'
-    Pusher.key = '3369c2bfacb59b4465df'
-    Pusher.secret = '9656e10dcafb7711405a'
+Pusher.app_id = '156778'
+Pusher.key = '511393ecea7f7523b756'
+Pusher.secret = '173537e991053fb10f41'
 
 Pusher.logger = Rails.logger
