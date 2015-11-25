bcc9fc6243f7417e7a7599d59fd1dbe5a14657e4
Keith Tang <keithsktang@gmail.com>
2015-11-24 20:58:16 -0500

    change production key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,9 @@ Rails.application.configure do
 
   require 'pusher'
 
-  Pusher.app_id = '156778'
-  Pusher.key = '511393ecea7f7523b756'
-  Pusher.secret = '173537e991053fb10f41'
+  Pusher.app_id = '156802'
+  Pusher.key = '0cdf7561a0fb9a4dcab5'
+  Pusher.secret = '8dc7c2b522e3b8789086'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -9,8 +9,14 @@ require 'pusher'
 # Pusher.key = '9a29dc21625531215e18'
 # Pusher.secret = 'b07b3be6f2f24a87c5de'
 
+# #KEYS FOR RADIANT-MESA-7219
 # Pusher.app_id = '156778'
 # Pusher.key = '511393ecea7f7523b756'
 # Pusher.secret = '173537e991053fb10f41'
+
+# #KEYS FOR STILL-PLATEAU-3220
+# Pusher.app_id = '156802'
+# Pusher.key = '0cdf7561a0fb9a4dcab5'
+# Pusher.secret = '8dc7c2b522e3b8789086'
 
 Pusher.logger = Rails.logger
