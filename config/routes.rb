931d89b744f245c80a5fb660e3b0f16e03c372c9
Keith Tang <keithsktang@gmail.com>
2015-11-25 14:00:46 -0500

    add welcome controller and welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
 
+  root 'welcome#index'
+
+
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-
-  root to: 'users#new'
-
   resources :messages
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
