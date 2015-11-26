b671833dcf850555922da87653e1586d33ea1abc
Keith Tang <keithsktang@gmail.com>
2015-11-25 20:26:58 -0500

    tidy up old codes

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -57,29 +57,29 @@ $(document).ready(function () {
         }
     });
 
-    // loading page content for navigation
-    $("a.navlink").live("click", function (e) {
-        e.preventDefault();
-        var linkurl = $(this).attr("href");
-        var linkhtmlurl = linkurl.substring(1, linkurl.length);
-
-        var imgloader = '<center style="margin-top: 30px;"><img src="img/preloader.gif" alt="loading..." /></center>';
-
-        closeMe();
-
-        $(function () {
-            topbar.css("top", "0px");
-            window.scrollTo(0, 1);
-        });
-
-        content.html(imgloader);
-
-        setTimeout(function () {
-            content.load(linkhtmlurl, function () {});
-                               }, 1200
-                  );
-    });
-});
+//     // loading page content for navigation
+//     $("a.navlink").live("click", function (e) {
+//         e.preventDefault();
+//         var linkurl = $(this).attr("href");
+//         var linkhtmlurl = linkurl.substring(1, linkurl.length);
+//
+//         // var imgloader = '<center style="margin-top: 30px;"><img src="img/preloader.gif" alt="loading..." /></center>';
+//
+//         closeMe();
+//
+//         $(function () {
+//             topbar.css("top", "0px");
+//             window.scrollTo(0, 1);
+//         });
+//
+//         content.html(imgloader);
+//
+//         setTimeout(function () {
+//             content.load(linkhtmlurl, function () {});
+//                                }, 1200
+//                   );
+//     });
+// });
 
 $(function () {
     var input = $('input[type=text]');
