fc37a1aee80e4093160e6331af92fb568190a7d6
jjakeobson <jjakeobson@gmail.com>
2015-11-28 17:15:13 -0500

    changed for heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider => 'AWS',       # required
-    :aws_access_key_id => ENV['S3_KEY'],       # required
-    :aws_secret_access_key => ENV['S3_SECRET'],
+    :s3_key => ENV['S3_KEY'],       # required
+    :s3_secret => ENV['S3_SECRET'],
     :region => ENV['S3_REGION']       # required
   }
   config.fog_directory  = ENV['S3_BUCKET_NAME']                     # required
