2fe04b3e0cc6da90021cf1a378b86d2f269886f5
jjakeobson <jjakeobson@gmail.com>
2015-11-27 19:13:38 -0500

    s3 working and chat working!

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',       # required
-    :aws_access_key_id      => ENV["S3_KEY"],       # required
-    :aws_secret_access_key  => ENV["S3_SECRET"],       # required
-    # :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+    :provider => 'AWS',       # required
+    :aws_access_key_id => ENV['S3_KEY'],       # required
+    :aws_secret_access_key => ENV['S3_SECRET'],
+    :region => ENV['S3_REGION']       # required
   }
-  config.fog_directory  = ENV["S3_BUCKET_NAME"]                     # required
+  config.fog_directory  = ENV['S3_BUCKET_NAME']                     # required
   # config.asset_host       = 'https://assets.example.com'            # optional, defaults to nil
   # config.fog_public     = false                                   # optional, defaults to true
   # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
